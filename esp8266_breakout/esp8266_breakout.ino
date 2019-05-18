04707b94a0e3e79db46f67cdc0b6efeab8c078ea
Nick Anderson <nanderson7@gmail.com>
2019-05-18 13:15:37 -0700

    sensor: temperature readouts now being posted to ThingSpeak

--- esp8266_breakout/esp8266_breakout.ino
+++ esp8266_breakout/esp8266_breakout.ino
@@ -1,62 +1,38 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266HTTPClient.h>
-
 #include <Stream.h>
-
 #include <WiFiClientSecureBearSSL.h>
-// I2C lib
-#include <Wire.h>
 
 // Wifi credential information
 const String kSsid = "Pwn135";
 const String kPassword = "my.little.pwnies";
 
-// Overall delay
-const int kDelay = 1000;
+// Overall delay, Thingspeak limits to 15 seconds
+const int kDelay = 20000;
 
 // Max size of our JSON doc for writing information
 const size_t kMaxJsonDoc = JSON_OBJECT_SIZE(4);
+StaticJsonDocument<kMaxJsonDoc> doc;
 
 // Init the ESP8266 Wifi Module
 ESP8266WiFiMulti WiFiMulti;
-
-// URI for remote resource we're grabbing
-const String kApiUri = "https://canhazip.com";
-const String kApiHost = "https://canhazip.com";
-const String kApiPort = "80";
-const char* kCanHazThumbprint = "39 11 70 BA 7D 5D D1 E0 3C 80 A8 5B B2 8F 28 57 67 A1 FC 4A";
 
 // Thingspeak API information
 const char* kThingSpeakThumbPrint = "F9 C2 65 6C F9 EF 7F 66 8B F7 35 FE 15 EA 82 9F 5F 55 54 3E";
 const String kThingSpeakUri = "https://api.thingspeak.com/update?";
 const String kThingSpeakApiKey = "api_key=2RJ5MY0HKIFM286P";
 const String kThingSpeakField1 = "&field1=";
-const String kThingSpeakField2 = "field2=";
+const String kThingSpeakField2 = "&field2=";
 
-// Global values used to receive data from i2c
-#define SDA_PIN 4
-#define SCL_PIN 5
-
-const int16_t I2C_MASTER = 0x42;
-const int16_t I2C_SLAVE = 0x08;
-
+// Helper buffer for receiving our Serial data
 char jsonBuff[0x100];
-boolean receiveFlag { false };
-
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // Configure I2C
-  Wire.begin(SDA_PIN, SCL_PIN, I2C_SLAVE);
-
-  // Attach a function to trigger when something is received.
-  Wire.onReceive(receiveEvent);
-
-  // Serial for debug logging
+  // Serial receiving data from 32u4
   Serial.begin(115200);
   
   // initialize digital pin LED_BUILTIN as an output.
@@ -67,82 +43,65 @@ void setup() {
   
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(500);
-    //Serial.print(" .");
   }
-
-  //Serial.println();
-  //Serial.print("Connected to ");
-  //Serial.print(kSsid);
-  //Serial.print(" with IP address: ");
-  //Serial.println(WiFi.localIP());
   memset(jsonBuff, 0, sizeof(jsonBuff));
-}
-
-// Our handler for receiving data via i2c
-void receiveEvent(size_t numBytes) {
-  int read = Wire.readBytes(jsonBuff, numBytes);
-  //Serial.println("Read bytes: " + String(read));
-  receiveFlag = true;
 }
 
 // Fetches a string response from a remote API endpoint URI
 int getApiResponse(const String& uri, String& resp) {
-  //Serial.println("Fetching resource from " + uri);
   std::unique_ptr<BearSSL::WiFiClientSecure>secure_client(new BearSSL::WiFiClientSecure);
   
-  secure_client->setFingerprint(kCanHazThumbprint);
+  secure_client->setFingerprint(kThingSpeakThumbPrint);
   secure_client->setTimeout(3000);
 
   HTTPClient https;
-  //Serial.println("Beginning connection to secure API endpoint");
   auto ret = https.begin(*secure_client, uri);
 
   if (!ret) {
-    //Serial.println("Failed to connect to URI with " + String(ret));
     return 1;
   }
   
-  //Serial.println("Sending HTTP GET request");
   auto status = https.GET();
   if (status != HTTP_CODE_OK) {
-    //Serial.println("HTTPS GET failed with code " + String(status));
     return status;
   }
 
   resp = https.getString();
-  //Serial.println("Successfully retrieved data from " + kApiUri);
-  //Serial.println(resp);
-
   return status;
 }
 
-void loop() {
-  StaticJsonDocument<kMaxJsonDoc> doc;
-  
-  // We blink every time we read the temperature for some form of feedback
+void blink() {
   digitalWrite(LED_BUILTIN, HIGH);
   delay(kDelay);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(kDelay);
+  delay(kDelay);  
+}
 
-  // If we've received a message, make an HTTPS request
-  //Serial.println("Checking if data has been received...");
-  Serial.readBytes(jsonBuff, 0x100);
-  //if (receiveFlag) {
-    //Serial.println("[+] Received buffer from 32u4: " + String(buff));
+void loop() {
+  // We blink every time we read the temperature for some form of feedback
+  //blink();
 
-    //String resp {""};
-    //auto ret = getApiResponse(kApiUri, resp);
-    //if (ret == 1) {
-      //Serial.println("Fetch remote resource failed. Continuing");
-    //} else {
-      //Serial.println("STUN IP: " + resp);
-  Serial.print("Received data");
+  // If we've received a message, POST the data to ThingSpeak
+  if (Serial.available() > 0) {
+    Serial.readBytes(jsonBuff, 0x100);
+    auto err = deserializeJson(doc, String(jsonBuff));
+    if (err) {
+      Serial.print("Failed to parse " + String(jsonBuff));
+      Serial.println(" error: " + String(err.c_str()));
+      return;
+    }
+    String uri = kThingSpeakUri + kThingSpeakApiKey + 
+                 kThingSpeakField1 + doc["temp"].as<String>() + 
+                 kThingSpeakField2 + doc["uptime"].as<String>();
+    String resp {""};
+    auto ret = getApiResponse(uri, resp);
+    Serial.println("POST " + String(jsonBuff) + " got back (" + 
+                   String(ret) + ", " + resp + ")");
+  } else {
+    Serial.println("No data yet, waiting...");
+  }
 
+  // Lastly clear out our buffer
   memset(jsonBuff, 0, sizeof(jsonBuff));
-      
-    //}
-  //receiveFlag = false;
-  //}
   delay(kDelay);
 }
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -4,9 +4,6 @@
 #include <OneWire.h>
 #include <SD.h>
 
-// I2C lib
-#include <Wire.h>
-
 // Data wire is plugged into pin 5 on the Arduino 
 #define ONE_WIRE_BUS 5
 
@@ -19,33 +16,21 @@ DallasTemperature sensors(&oneWire);
 // Pinout that we get SD card info from
 const int kCardSelect = 4;
 
-// Overall delay
-const int kSensorDelay = 1000;
+// Overall delay, Thingspeak limits to 15 seconds
+const int kDelay = 20000;
 
 // Total filename must be < 8 chars, because FAT
 const String kLogSuffix = "temp.log";
 
 // Max size of our JSON doc for writing information
 const size_t kMaxJsonDoc = JSON_OBJECT_SIZE(3);
-
-// Define our RX/TX pins, TODO we need to verify these
-//#define RX 0
-//#define TX 1
-//SoftwareSerial toEsp(RX,TX);
-
-#define I2C_ADDRESS 9
-const int16_t I2C_MASTER = 0x42;
-const int16_t I2C_SLAVE = 0x08;
-
-char buff[0x100];
+StaticJsonDocument<kMaxJsonDoc> doc;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // Startup I2C
-  Wire.begin();
-
   // Serial for debug logging
   Serial.begin(115200);
+  // Serial for communicating with esp
   Serial1.begin(115200);
   
   // initialize digital pin LED_BUILTIN as an output.
@@ -57,9 +42,7 @@ void setup() {
   }
 
   sensors.begin();
-  Serial.println("Initialization complete");
-
-
+  Serial.println("Initialization complete");  
 }
 
 // Helper function to write a log line to disk
@@ -75,32 +58,19 @@ void log_data_to_disk(const String& msg, const String& fname) {
 
 // Helper function to POST a log line to our SIEM
 void log_data_to_siem(const String& msg) {
-  Serial.println("Sending JSON payload to ESP8266 over i2c");
-  //Wire.beginTransmission(I2C_SLAVE);
-  char* bytes = msg.c_str();
-  Serial1.write(bytes, msg.length());
-  //int ret = Wire.write(msg.c_str());
-  Serial.println("Wrote " + String(msg.length()) + " bytes");
-  //Wire.endTransmission();
-  Serial1.readBytes(buff, 0x100);
-  Serial.println("Got back: " + String(buff));
+  Serial.println("Sending " + msg + " to esp");
+  // Send our payload to the ESP8266
+  Serial1.print(msg);
+}
+
+void blink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(kDelay);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(kDelay);  
 }
 
 void loop() {
-  // Try to find out what our I2C address is for esp8266
-  for (int address = 1; address <= 255; address++) {
-    //Serial.println("Scanning for i2c devices on "+ String(address));
-    Wire.beginTransmission(address); // Select address
-    int err = Wire.endTransmission();
-    if (err == 0) {
-      Serial.println("Found device at: " + String(address));
-    } else {
-      //Serial.println("Device transmit error: " + String(err));
-    }
-  }
-  
-  StaticJsonDocument<kMaxJsonDoc> doc;
-  
   // User a naive hour-based uptime prexif ro our log file
   // TODO: Use the ESP8266 to get the current time from an NTP server or something
   auto uptime = millis();
@@ -108,10 +78,7 @@ void loop() {
   auto fname = String(hour) + "-" + kLogSuffix;
   
   // We blink every time we read the temperature for some form of feedback
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(kSensorDelay);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(kSensorDelay);
+  //blink();
 
   // Send the command to get temperature readings
   sensors.requestTemperatures();
@@ -128,5 +95,5 @@ void loop() {
 
   log_data_to_siem(json);
 
-  delay(kSensorDelay);
+  delay(kDelay);
 }
